31f72fdf595a2f1b491eb97dd18ab1308db80f02
RockPalmer <77813599+RockPalmer@users.noreply.github.com>
2021-05-29 16:43:10 -0700

    Layout was altered to use more dynamic layout managers

--- My Calculator/calcGUI/QuadraticFormula.java
+++ My Calculator/calcGUI/QuadraticFormula.java
@@ -1,108 +1,92 @@
 package calcGUI;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class QuadraticFormula extends BaseGUI implements ActionListener
+public class QuadraticFormula extends BaseGUI
 {
-	private static JButton button;
-	protected static JTextField[] textFields;
-	protected static JTextField answerField;
-	protected static JLabel[] labels;
-	public static void buildGUI()
+	private JTextField[] textFields;
+	private JPanel mainPanel;
+	public QuadraticFormula() 
 	{
-		/* Clear panel */
-		panel.removeAll();
-		panel.repaint();
+		//Initialize instance variables
+		final Dimension TEXT_FIELD_SIZE = new Dimension(60,20);
+		mainPanel = new JPanel();
+		textFields = new JTextField[4];
+		JPanel dataPanel = new JPanel();
+		JPanel optionsPanel = new JPanel();
+		JPanel titlePanel = new JPanel();
+		JButton menuButton = new JButton();
+		JButton calculate = new JButton();
+		JLabel title = new JLabel();
 		
-		setErrWarning();
-		buildButton();
-		buildTextFields();
-		buildLabels();
-	}
-	public void actionPerformed(ActionEvent arg0) 
-	{
-		buildGUI();
-	}
-	private static void buildButton() 
-	{
-		setMenuButton();
+		//Build layout of mainPanel
+		mainPanel.setLayout(new BorderLayout());
+		mainPanel.add(titlePanel, BorderLayout.PAGE_START);
+		mainPanel.add(dataPanel, BorderLayout.CENTER);
+		mainPanel.add(optionsPanel, BorderLayout.PAGE_END);
 		
-		button = new JButton();
-		panel.add(button);
+		//Build layout of titlePanel
+		titlePanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+		title.setText("Quadratic Formula");
+		titlePanel.add(title);
 		
-		button.setText("Calculate");
-		setOnPanel(button, 4, 500);
-		
-		/* Add actionListener to button to calculate results */
-		button.addActionListener(new calculate());
-	}
-	private static void buildTextFields() 
-	{
-		buildInputFields();
-		buildAnswerField();
-	}
-	private static void buildInputFields() 
-	{
-		textFields = new JTextField[3];
-		
-		/* Fill the textField array and place the textFields onto the panel */
-		for (int count = 0; count < textFields.length; count++) 
-			textFields[count] = new JTextField();
-		addComponentsToPanel(textFields);			
-		/* Set the size of each textField to be 100 wide and 26 tall */
-		for (int count = 0; count < textFields.length; count++)
+		//Build layout of dataPanel
+		dataPanel.setLayout(new BoxLayout(dataPanel, BoxLayout.Y_AXIS));
+		String[] labels = {"a =","b =","c =","x ="};
+		for (int count = 0; count < 4; count++) 
 		{
-			textFields[count].setSize(TEXT_FIELD_WIDTH, TEXT_FIELD_HEIGHT);
-			textFields[count].setLocation(COLUMN_COORDS[2] + 50 - (TEXT_FIELD_WIDTH/2), 40 + count * 30);
+			JPanel p = new JPanel();
+			p.setLayout(new FlowLayout(FlowLayout.CENTER));
+			p.add(new JLabel(labels[count]));
+			JTextField t = new JTextField();
+			t.setPreferredSize(TEXT_FIELD_SIZE);
+			p.add(t);
+			textFields[count] = t;
+			dataPanel.add(p);
 		}
+		
+		//Build layout of optionsPanel
+		optionsPanel.setLayout(new BoxLayout(optionsPanel, BoxLayout.X_AXIS));
+		menuButton.setText("Return to Menu");
+		calculate.setText("Calculate");
+		menuButton.addActionListener(main);
+		calculate.addActionListener(new Calculate());
+		optionsPanel.add(Box.createHorizontalGlue());
+		optionsPanel.add(menuButton);
+		optionsPanel.add(Box.createHorizontalGlue());
+		optionsPanel.add(calculate);
+		optionsPanel.add(Box.createHorizontalGlue());
+		
+		centerPanel.add(mainPanel, "Quadratic Formula");
 	}
-	private static void buildAnswerField() 
+	public void show()
 	{
-		answerField = new JTextField();
-		
-		answerField.setSize(TEXT_FIELD_WIDTH, TEXT_FIELD_HEIGHT);
-		answerField.setLocation(COLUMN_COORDS[2] + 50 - (TEXT_FIELD_WIDTH)/2, textFields[2].getY() + 30);
-		
-		panel.add(answerField);
+		mainLayout.show(centerPanel, "Quadratic Formula");
 	}
-	private static void buildLabels() 
-	{
-		labels = new JLabel[4];
-		
-		/* Fill labels array and place the labels onto the panel */
-		for (int count = 0; count < labels.length; count++)
-			labels[count] = new JLabel();
-		addComponentsToPanel(labels);
-		
-		/* Set the text of the labels to be the 3 variables that the user will
-		 * input and the last one as the 'x' the user is looking for */
-		labels[0].setText("a = ");
-		labels[1].setText("b = ");
-		labels[2].setText("c = ");
-		labels[3].setText("x = ");
-		
-		/* Set the size of each label based on the text in each one */
-		setComponentsSize(labels);
-		
-		/* Set the locations of the various labels and textField to appear in 2 
-		 * columns next to each other*/
-		for (int count = 0; count < labels.length; count++) 
-			labels[count].setLocation(COLUMN_COORDS[2] - 50 - (labels[count].getWidth()/2), 40 + count * 30);
-	}
-	private static class calculate implements ActionListener
+	private class Calculate implements ActionListener
 	{
 		public void actionPerformed(ActionEvent arg0) 
 		{
-			if (calcErr.TextFieldErr.isValidInput(textFields, errWarning)) 
+			try
 			{
 				double x1 = calcFn.QuadFormFn.calculate(Double.parseDouble(textFields[0].getText()), Double.parseDouble(textFields[1].getText()), Double.parseDouble(textFields[2].getText()))[0];
 				double x2 = calcFn.QuadFormFn.calculate(Double.parseDouble(textFields[0].getText()), Double.parseDouble(textFields[1].getText()), Double.parseDouble(textFields[2].getText()))[1];
-				answerField.setText(x1 + " , " + x2);
+				textFields[3].setText(x1 + " , " + x2);
+			}
+			catch (NumberFormatException e) 
+			{
+				errWarning[0].setText("ERROR");
 			}
 		}
 	}
